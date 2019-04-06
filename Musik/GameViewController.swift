d7a599dbdbe28908e5d6afc7e6bc99c63c9384b2
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-04-06 19:25:15 -0300

    Initial Commit

---
+++ Musik/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  Musik
+//
+//  Created by Vinicius Mangueira Correia on 06/04/19.
+//  Copyright © 2019 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
